8d1f6b1d928f340fa1062b5daef96b28975259d5
Felipe Rosa <flp.far@gmail.com>
2020-05-01 13:47:40 -0300

    Add form to new.html and filled the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      p @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
   end
 end
