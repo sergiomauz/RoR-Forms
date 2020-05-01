394f9e5c1efa2bf9e285784d504cf40aa5b5af50
Felipe Rosa <flp.far@gmail.com>
2020-05-01 16:49:11 -0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    validates :email, :username, uniqueness: true, presence: true
-    validates :username, length: { in: 4..15 }
-    validates :password, presence: true, length: { minimum: 6 }
-        
+  validates :email, :username, uniqueness: true, presence: true
+  validates :username, length: { in: 4..15 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
