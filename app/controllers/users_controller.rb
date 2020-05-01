e553f46b565d3d7e4d4425b6861ec6b12d56b634
Felipe Rosa <flp.far@gmail.com>
2020-05-01 16:39:42 -0300

    Fix linter error and remove comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -7,9 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # @user.email = params[:email]
-    # @user.username = params[:username]
-    # @user.password = params[:password]
 
     if @user.save
       redirect_to new_user_path
