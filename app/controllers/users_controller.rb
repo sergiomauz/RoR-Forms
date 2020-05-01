5641001f762d210befadb4de722fb82cda3519e5
sergiomauz <sergio.mauz88@gmail.com>
2020-05-01 12:59:53 -0500

    Added editing and adding forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,34 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+    #@user.email = params[:email]
+    #@user.username = params[:username]
+    #@user.password = params[:password]
 
     if @user.save
       redirect_to new_user_path
     else
       p @user.errors.full_messages
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)        
+    else
+      p @user.errors.full_messages
+      render :edit
     end
   end
 
