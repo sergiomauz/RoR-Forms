ad9ac82c6ec81892da966558d6c36c51c768247e
Felipe Rosa <flp.far@gmail.com>
2020-05-04 16:37:48 -0300

    Remove whitespace - linter error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
     # @user.email = params[:email]
     # @user.username = params[:username]
     # @user.password = params[:password]
